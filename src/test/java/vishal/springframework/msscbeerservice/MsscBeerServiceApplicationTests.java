9616ed6d128d4b5372858e37e74981065ff5ad74
vishchan <vishalchandrola20@gmail.com>
2021-02-24 00:27:31 +0530

    Controller tests created closes#4

--- src/test/java/vishal/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
+++ src/test/java/vishal/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -1,10 +1,13 @@
 package vishal.springframework.msscbeerservice;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MsscBeerServiceApplicationTests {
 
-
+    @Test
+    void contextLoads() {
+    }
 
 }
